1103df8ff9a4209478cf57bf3508f5dad4a18ef0
Dee Vazquez <dee.quez11@gmail.com>
2023-01-23 08:01:36 -0800

    communicate via channel

--- main.go
+++ main.go
@@ -1,16 +1,15 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
+import "fmt"
 
 func main() {
-	go f()
-	time.Sleep(1 * time.Second)
-	fmt.Println("main function uses concurrency with f function")
+	c := make(chan int)
+	go strlen("Salutations", c)
+	go strlen("World", c)
+	x, y := <-c, <-c
+	fmt.Println(x, y, x+y)
 }
 
-func f() {
-	fmt.Println("f function")
+func strlen(s string, c chan int) {
+	c <- len(s)
 }

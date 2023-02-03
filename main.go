b6b82b635b0185afc16ad65b0111e9add17b0a0e
Dee Vazquez <dee.quez11@gmail.com>
2023-02-03 14:25:49 -0800

    test

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ type Foo struct {
 }
 
 func main() {
-	f := Foo{"Joe Junior", "Hello Shabado test"}
+	f := Foo{"Joe Junior", "Hello Shabado"}
 	b, _ := json.Marshal(f)
 	fmt.Println(string(b))
 	json.Unmarshal(b, &f)
